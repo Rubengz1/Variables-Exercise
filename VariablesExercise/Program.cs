228491780141397a0b0bffec5de1c7313f7c1e86
Ruben Gonzalez <rubengz23@yahoo.com>
2026-02-02 20:15:54 -0800

    Variables, save

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string firstName = "Bob";
+            int age = 20;
+            char initial = 'G';
+            bool isMale = true;
+            double currentWeight = 250.5;
+            decimal targetWeight = 200.1m;
+
+            Console.WriteLine($"{firstName} {initial} : {age} years old");
+            Console.WriteLine($"He is male : {isMale} ");
+            Console.WriteLine($"Current weight is {currentWeight} pounds, and his target weight is {targetWeight} pounds");
+
+        }
         }
     }
-}
+
